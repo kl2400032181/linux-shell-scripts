e73c406b27884f4b9f28702936fa969721482ecf
kl2400032181 <2400032181@kluniversity.in>
2025-09-26 23:09:35 +0530

    Add Looping with Numbers.sh
    Add Looping with Numbers.sh

---
+++ Looping with Numbers.sh
@@ -0,0 +1,24 @@
+Use a for loop to display the natural numbers from  to .
+
+Input Format
+
+There is no input
+
+Output Format
+
+1
+2
+3
+4
+5
+.
+.
+.
+.
+.
+50
+Answer: #!/bin/bash
+for((i=1;i<=50;i++))
+do 
+echo "$i"
+done
